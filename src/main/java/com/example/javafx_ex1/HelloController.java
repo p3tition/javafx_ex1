f77e2d8d180fc7fcfc78530c42414f4cfe9e9c02
p3tition <saibankanseizaki@gmail.com>
2022-12-06 23:20:23 +0100

    t1

--- src/main/java/com/example/javafx_ex1/HelloController.java
+++ src/main/java/com/example/javafx_ex1/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.javafx_ex1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField fxusername;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField fxpassword;
+    @FXML
+    public void click(ActionEvent e) {
+        String username = fxusername.getText();
+        String password = fxpassword.getText();
+        System.out.println(username);
+        System.out.println(password);
     }
 }
